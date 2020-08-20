72a779b8341b7d738d2ebd543baff3835b083080
Olive Iosello <oiosello@gmail.com>
2020-08-20 16:52:12 -0700

    Made all tests pass for Prototypal instantiation style

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,9 +1,36 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(queueMethods);
+  instance.count = 0;
+  instance.storage = {};
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  //write methods for queue to use
+  enqueue: function(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  },
+  dequeue: function(value) {
+    if (this.count === 0) {
+      return undefined;
+    }
+    let deletedItem = this.storage[0];
+    for (var i = 0; i < this.count; i++) {
+      this.storage[i] = this.storage[i + 1];
+    }
+
+    delete this.storage[this.count - 1];
+    this.count--;
+
+    return deletedItem;
+  },
+  size: function(value) {
+    return this.count;
+  }
+};
 
 //use obj.create
 //could make first and last global objects to return using this
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,9 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(stackMethods);
+  instance.count = 0;
+  instance.storage = {};
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  },
+  pop: function() {
+    if (this.count === 0) {
+      return undefined;
+    }
+    let index = this.count;
+    let deletedItem = this.storage[index - 1];
+    delete this.storage[index];
+    this.count--;
+    return deletedItem;
+  },
+  size: function() {
+    return this.count;
+  }
+};
+
 
 //use obj.create
 //could make first and last global objects to return using this
