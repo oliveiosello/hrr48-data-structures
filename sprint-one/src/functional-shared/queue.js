5d6cd58eff55e43a42646496862714f0e5b8f97b
Olive Iosello <oiosello@gmail.com>
2020-08-20 16:43:31 -0700

    Made all tests pass for Functional-shared instantiation style

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,10 +1,39 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {
+    count: 0,
+    storage: {}
+  };
+
+  _.extend(instance, queueMethods);
+
+  return instance;
 };
 
 var queueMethods = {
   //write methods for queue to use
+  enqueue: function(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  },
+  dequeue: function(value) {
+    if (this.count === 0) {
+      return undefined;
+    }
+    let deletedItem = this.storage[0];
+    for (var i = 0; i < this.count; i++) {
+      this.storage[i] = this.storage[i + 1];
+    }
+
+    delete this.storage[this.count - 1];
+    this.count--;
+
+    return deletedItem;
+  },
+  size: function(value) {
+    return this.count;
+  }
 };
 
 //use this and _.extend
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,10 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {
+    count: 0,
+    storage: {}
+  };
+  _.extend(instance, stackMethods);
+  return instance;
 };
 
 var stackMethods = {
   //write methods for queue to use
+  push: function(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  },
+  pop: function() {
+    if (this.count === 0) {
+      return undefined;
+    }
+    let index = this.count;
+    let deletedItem = this.storage[index - 1];
+    delete this.storage[index];
+    this.count--;
+    return deletedItem;
+  },
+  size: function() {
+    return this.count;
+  }
 };
 
 //use this and _.extend
