d419b6c4fbb2220cbd09090918d7dfbf21914a80
Olive Iosello <oiosello@gmail.com>
2020-08-20 17:03:17 -0700

    Made all tests pass for Pseudoclassical instantiation style. Assignment complete.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.storage = {};
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
 
+Queue.prototype.dequeue = function() {
+  if (this.count === 0) {
+    return undefined;
+  }
+  let deletedItem = this.storage[0];
+  for (var i = 0; i < this.count; i++) {
+    this.storage[i] = this.storage[i + 1];
+  }
+
+  delete this.storage[this.count - 1];
+  this.count--;
+
+  return deletedItem;
+};
+
+Queue.prototype.size = function() {
+  return this.count;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.storage = {};
+};
+
+Stack.prototype.push = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
+
+Stack.prototype.pop = function() {
+  if (this.count === 0) {
+    return undefined;
+  }
+  let index = this.count;
+  let deletedItem = this.storage[index - 1];
+  delete this.storage[index];
+  this.count--;
+  return deletedItem;
+};
+
+Stack.prototype.size = function() {
+  return this.count;
 };
 
 
