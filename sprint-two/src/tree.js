0082bdf5ed0a22ebb28efeeff40229ea6ce1f83e
Owen Wexler <owenbwexler@gmail.com>
2020-08-21 18:47:47 -0400

    Implement addChild on tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,14 +3,21 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
 
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  // create variable tree to push set to empty tree
+  // give new tree value of function
+  // push a new tree obj to children array
+  let treeToPush = Tree(value);
+  this.children.push(treeToPush);
+
 };
 
 treeMethods.contains = function(target) {
@@ -21,3 +28,25 @@ treeMethods.contains = function(target) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+/*
+
+{value: 1,
+chilren: [
+  {value: 2,
+  children: [
+    {value: 4,
+    children: []},
+    {value: 5,
+    children: []]},
+  {value: 3,
+  children: [
+    {value: 6,
+    children: []},
+    {value: 7,
+    children: []}
+  ]},
+  ]
+]};
+
+*/
