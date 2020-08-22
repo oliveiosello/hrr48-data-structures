4213416e0ee029c308d0b3a6de5d652bcf689016
Owen Wexler <owenbwexler@gmail.com>
2020-08-22 19:29:49 -0400

    Implement addNode, contains, and removeNode on Graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,18 +2,38 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  // adjacencyList: object of arrays
+  // {A: [B, C],
+  //  B: [A],
+  //  C: [A],
+  //  D: [A, C]}
+  this.adjacencyList = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  // add a k-v pair to adjacencyList
+  this.adjacencyList[node] = [];
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return this.adjacencyList[node] !== undefined;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  if (!this.contains(node)) {
+    return;
+  }
+
+  // remove the node (meaning the key from the a list)
+  // go through all of the rest of the keys and filter that value out of all the arrays
+  delete this.adjacencyList[node];
+
+  for (let adjacencyArray in this.adjacencyList) {
+    adjacencyArray = adjacencyArray.filter(element => element !== node);
+  }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
