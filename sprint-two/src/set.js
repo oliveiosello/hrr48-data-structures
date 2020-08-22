2f2b1207f65ac4f8aed96a9aa4e9a0ca8dd676b2
Owen Wexler <owenbwexler@gmail.com>
2020-08-21 21:32:22 -0400

    Implement Set in its entirety

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,32 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  // if item does not exist in storage
+  //// add item as a key to storage with a value of 1
+  // if it does
+  //// increment the value at the key by 1
+  if (this._storage[item]) {
+    this._storage[item] += 1;
+  } else {
+    this._storage[item] = 1;
+  }
+
 };
 
 setPrototype.contains = function(item) {
+  return this._storage[item] !== undefined;
 };
 
 setPrototype.remove = function(item) {
+  let deletedItem = this._storage[item];
+  delete this._storage[item];
+  return deletedItem;
 };
 
 /*
